03024a5b26bce6f137efe9db062a1766136197ba
Jeremy Gygi <jeremy.gygi@yale.edu>
2021-05-20 12:12:42 -0400

    Updated cv.spear function

--- R/spear.R
+++ R/spear.R
@@ -226,7 +226,8 @@ pattern_samples = NULL, pattern_features = NULL,
     post_pis[,,,idx_w] = post_pi
     post_selections[,,idx_w] = post_tpiX
     post_selections_marginal[,,idx_w]  = post_tpiX_marginal
-    cat(paste0("*** weights: ",ws[idx_w], "------------------------"))
+    
+    cat(paste0("*** ", SPEAR.color_text(paste0("Running w = ", ws[idx_w]), "green"), "------------------------\n"))
   }
   post_selections_joint = ifelse(post_selections<=post_selections_marginal, post_selections, post_selections_marginal)
   # hist(post_selections_marginal[,1,idx_w], breaks = 100)

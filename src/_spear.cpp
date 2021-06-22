b2f381e264409d7b8f9e8479bd0a9b3eece5ecf7
Jeremy Gygi <jeremy.gygi@yale.edu>
2021-06-22 14:03:30 -0400

    updating R files

--- src/_spear.cpp
+++ src/_spear.cpp
@@ -284,7 +284,7 @@ arma::mat spear_(const int family, arma::mat& Y,  arma::mat& X,
         }
         if(print_out != 0){
           if((it > warm_up) & ((it - warm_up)% print_out == 0)){
-              Rcout << "iter" << it - warm_up << " - ΔELBO=" << Delta << "\n";
+              Rcout << "iter" << it - warm_up << "\t| ΔELBO=" << Delta << "\t| thresh=" << thres_elbo << "\n";
           }
         }
         it += 1;
